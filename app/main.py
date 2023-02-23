388a61696a6c90f4a0c39a9c700f48a09fa8df82
smark-ark <arkhangreat2@gmail.com>
2023-02-23 21:48:45 +0530

    cors masla 2

--- app/main.py
+++ app/main.py
@@ -14,11 +14,9 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
